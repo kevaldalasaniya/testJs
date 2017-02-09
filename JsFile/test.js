49469e9bdef17273b7b96a5570c3c03eab13f22f
keval <keval@toweya.com>
2017-02-09 23:47:48 +0530

    update

--- JsFile/test.js
+++ JsFile/test.js
@@ -9,7 +9,7 @@ function boris_media_group_getSuggestion(id) {
 	var x = document.querySelector('meta[name="keywords"]');
 	var title = document.title.replace(/\u2013|\u2014/g, "-"); 
 	
-	var server = "http://45.79.71.25:8080/trafficSharing/getSuggestion?keywords="
+	var server = "http://localhost:8080/trafficSharing/getSuggestion?keywords="
 			+ escape(x.content)+ "&detail="+escape((title)) + "&id=" + id + "&host=" + escape(window.location.host)
 			+ "&href=" + escape(window.location.href) ;
 	
@@ -24,7 +24,7 @@ function boris_media_group_getSuggestion(id) {
 			var detail = jsonResponse.detail;
 			console.log(jsonResponse);
 			var html = "<div style=\"height: 60px; width: 468px;cursor:pointer;display: inline-block;text-align: left;\"  onclick=\"window.open(\'"+location+"\',\'_blank\')\" ><table>	<tr><td style=\"font-size: larger; color: #1a0dab\">"+detail+"</td></tr><tr><td style=\"font-size: small; color:green\"><span style=\"background-color:#59946b;	border-radius: 2px;	color: #fff;display: inline-block;	font-size: 12px;padding: 0 2px;line-height: 14px;vertical-align: baseline;\">Ad</span>&nbsp;"+extractDomain(location)+"</td></tr></table></div>";
-			document.getElementById("boris_media_group_getSuggestion").innerHTML = html;
+			document.getElementById("boris_media_group_suggestion").innerHTML = html;
 			
 		}
 	};
